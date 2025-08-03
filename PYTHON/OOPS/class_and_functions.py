f93de9d570280a2788c97c6d12076e248ee6b4e3
Sai kumar <pandisaikumar.tech@gmail.com>
2025-08-03 15:03:28 +0530

    classes and functions

---
+++ PYTHON/OOPS/class_and_functions.py
@@ -0,0 +1,22 @@
+#class and a function 
+
+class sample:
+    x=10
+    y=20
+    print(x)
+    print(y)
+    z=x+y 
+    print(z)
+
+print(sample.x)
+print(sample.y)
+
+
+def test():
+    a=30
+    b=20
+    print(a)
+    print(b)
+
+#without a function call, a function cannot be executed 
+#NOTE: SV is no way related to object, so even without object u can access a class 
